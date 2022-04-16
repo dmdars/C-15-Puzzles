765e5e177b93c3dd8b20af61ce7f6c0619ea6f92
dmdars <45941499+dmdars@users.noreply.github.com>
2022-04-16 17:15:25 +0700

    initial upload

---
+++ menu.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "userlib.h"
+//this is the progran to change the menu
+char arrow;
---
+++ userlib.h
@@ -0,0 +1,12 @@
+#ifndef userlib
+#define userlib
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+
+
+#endif // userlib
